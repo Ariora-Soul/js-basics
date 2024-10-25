f60639796bc757f5e3a4ec1a74c5adf1e6711e96
Ariora <146632578+Ariorasoul@users.noreply.github.com>
2024-10-25 16:48:47 +0200

    Js First Example incomplete (I don't know how to check when a variable is a number)

---
+++ script.js
@@ -0,0 +1,45 @@
+// Example 1: Ask 2 two numbers and add them with + and tall the result
+
+let numberAsked1 = parseInt(prompt("Put one number"));
+
+console.log("Number Asked Valor = " + numberAsked1);
+
+let numberAsked2 = parseInt(prompt("Put another"));
+
+console.log("Number Asked Valor = 2" + numberAsked2);
+
+alert("The addition of them is: " + (numberAsked1 + numberAsked2));
+
+console.log("End Example 1");
+
+// Example 2: Continues with ifs. when user don't type a number enters in to a while.
+
+/* 
+
+Unable to complete this part attm
+
+if (numberAsked1 == (String) || numberAsked2 == (String))  {
+
+alert("You dind't put a number on both promts")
+
+} */
+
+//Example 2: This examplen ask a country that you like and tell you some things .
+
+let cointryYouLike = prompt("What's country do you like?");
+
+console.log("Country Asked Valor = " + cointryYouLike);
+
+alert(cointryYouLike + "!" + " Thats awesome! I like Italy");
+
+let likeItaly = prompt("Do you Like?");
+
+if (likeItaly == "yes") {
+  alert("Nice!");
+} else if (likeItaly == "Yes") {
+  alert("Nice!");
+} else if (likeItaly == "No") {
+  alert("Oh, that's a same!");
+} else if (likeItaly == "no") {
+  alert("Oh, that's a same!");
+}
